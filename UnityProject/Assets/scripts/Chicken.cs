fed99f9fa2093e3a13482d09ea9172b826812f6f
ben33779610 <49440361+ben33779610@users.noreply.github.com>
2019-11-19 17:51:34 +0800

    c#pratice

---
+++ UnityProject/Assets/scripts/Chicken.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chicken : MonoBehaviour
+{
+	[Header("移動速度")][Range(20,200)]
+	public float speed;
+	[Header("跳躍高度")][Range(0,500)]
+	public int jump;
+	[Header("對話內容")]
+	public string talk = ("咕咕咕");
+
+    void Start()
+    {
+		print("Game Start"); //印出一次Start
+    }
+
+    
+    void Update()
+    {
+		print(talk);		//重覆對話內容
+    }
+}
